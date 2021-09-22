594f41474bb6f775941be270c1f363397d388694
li.linze.ex <li.linze@tci-cn.co.jp>
2021-09-22 15:06:41 +0900

    cores

--- pages/api/read-file.js
+++ pages/api/read-file.js
@@ -1,4 +1,24 @@
-export default (req, res) => {
+import Cors from 'cors';
+
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+});
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+export default async (req, res) => {
+  // Run the middleware
+  await runMiddleware(req, res, cors);
   res.json({
     file: req.body.substring(
       req.body.indexOf('application/octet-stream\r\n\r\n') + 28,
